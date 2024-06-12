74d1b1ad0c0eaa233af23af09663b95c028eccf2
Taha Zoabi <zoabi03@gmail.com>
2024-06-12 11:32:38 +0300

    trey

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,30 @@
 'use strict'
 
+// declare variables that are needed for the DOM
+const fullForm = document.querySelector(".form-container");
+const modalForm = document.querySelector(".modal-overlay");
+
+// function to open/show form
+const openModal = () => {
+    fullForm.style.display = "block";
+    modalForm.style.display = "block";
+};
+
+// function to close/hide form
+const closeModal = () => {
+    fullForm.style.display = "none";
+    modalForm.style.display = "none";
+};
+
+// event handler for add contact btn to open form
+showFormBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    clearInputFields(); // reset the inputs to empty strings
+    openModal();
+});
+
+// event handler for close form btn to hide form
+closeFormBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    closeModal();
+});
