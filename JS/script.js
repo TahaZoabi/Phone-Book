b7249eb00c603d922169bde00867e4529ee554db
Taha Zoabi <zoabi03@gmail.com>
2024-06-14 14:51:17 +0300

    js
    just tested that opening and clsoing modals is working
    
    gonna need to update all the names and add functionality later

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,14 @@
 "use strict";
 
 const showFormBtn = document.getElementById("showFormBtn");
-const hideFormBtn = document.getElementById("cancelBtn");
+const hideFormBtn = document.querySelector(".cancelBtn");
 const contactForm = document.querySelector(".contact-modal");
+const editBtn = document.getElementById("editBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const confirmBtn = document.querySelector(".confirm-btn");
+const confirmModal = document.getElementById("confirm-popup");
+const cancelBtnConfirmModal = document.getElementById("confirm-cancelBtn");
+const confirmBtnConfirmModal = document.getElementById("confirm-confirmBtn");
 
 const openModal = () => {
   contactForm.style.display = "flex";
@@ -10,6 +16,25 @@ const openModal = () => {
 const closeModal = () => {
   contactForm.style.display = "none";
 };
+
+const openConfirmModal = () => {
+  confirmModal.style.display = "flex";
+};
+const closeConfirmModal = () => {
+  confirmModal.style.display = "none";
+};
+
+const actions = () => {
+  // do shit later
+  console.log("actions were made");
+  closeConfirmModal();
+};
+
+confirmBtn.addEventListener("click", actions);
+
+editBtn.addEventListener("click", openModal);
+deleteBtn.addEventListener("click", openConfirmModal);
+cancelBtnConfirmModal.addEventListener("click", closeConfirmModal);
 
 showFormBtn.addEventListener("click", (e) => {
   openModal();
